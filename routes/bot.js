964abfd9a6fc80bcde7bb22fceaf2ab7d8e80a46
Arthelon <hsing.daniel@gmail.com>
2016-08-06 19:51:25 +0800

    Setup handler and message logging for fb messages webhook, wrote util scripts for sending fb messages to user

--- routes/bot.js
+++ routes/bot.js
@@ -1,9 +1,10 @@
 var router = require("express").Router()
+var fbMessage = require("../utils").fbMessage
 
 router.route("/fb/messages")
     .get(function (req, res) {
         if (req.query['hub.mode'] === 'subscribe' &&
-            req.query['hub.verify_token'] === process.env.VALIDATION_TOKEN) {
+            req.query['hub.verify_token'] === process.env.FB_VERIFY_TOKEN) {
             console.log("Validating webhook");
             res.status(200).send(req.query['hub.challenge']);
         } else {
@@ -12,14 +13,64 @@ router.route("/fb/messages")
         }
     })
     .post(function (req, res) {
-        var data = req.body;
+        // Message handler
+        app.post('/webhook', function (req, res) {
+            // Parse the Messenger payload
+            // See the Webhook reference
+            // https://developers.facebook.com/docs/messenger-platform/webhook-reference
+            var data = req.body;
 
-        if (data.object == 'page') {
-            data.entry.forEach(function (pageEntry) {
-                console.log(pageEntry)
-            });
+            if (data.object === 'page') {
+                data.entry.forEach(function (entry) {
+                    entry.messaging.forEach(function (event) {
+                        if (event.message) {
+                            const sender = event.sender.id;
+                            const sessionId = findOrCreateSession(sender);
+
+                            var text = event.message.text
+                            var attachments = event.message.attachments
+
+                            if (attachments) {
+                                // We received an attachment
+                                fbMessage(sender, 'Sorry I can only process text messages for now.')
+                                    .catch(console.error);
+                            } else if (text) {
+                                // We received a text message
+                                console.log("Message received:" + text)
+                                fbMessage(sender, "Message received")
+                                wit.converse(sessionId, text, sessions[sessionId].context)
+                                    .then(function(data) {
+                                        console.log("Received response from wit: " + data)
+                                    })
+                                // wit.runActions(
+                                //     sessionId, // the user's current session
+                                //     text, // the user's message
+                                //     sessions[sessionId].context // the user's current session state
+                                // ).then(function (context) {
+                                //     console.log('Waiting for next user messages');
+                                //
+                                //     // Based on the session state, you might want to reset the session.
+                                //     // This depends heavily on the business logic of your bot.
+                                //     // Example:
+                                //     // if (context['done']) {
+                                //     //   delete sessions[sessionId];
+                                //     // }
+                                //
+                                //     sessions[sessionId].context = context;
+                                // })
+                                .catch(function(err) {
+                                    console.error('Oops! Got an error from Wit: ', err.stack || err);
+                                })
+                            }
+                        } else {
+                            console.log('received event', JSON.stringify(event));
+                        }
+                    });
+                });
+            }
             res.sendStatus(200);
-        }
+        });
+
     })
 
 module.exports = router
---
+++ utils.js
@@ -0,0 +1,24 @@
+var utils = {}
+
+utils.fbMessage = function(id, text) {
+    const body = JSON.stringify({
+        recipient: { id: id },
+        message: { text: text },
+    });
+    const qs = 'access_token=' + encodeURIComponent(FB_PAGE_TOKEN);
+    return fetch('https://graph.facebook.com/me/messages?' + qs, {
+            method: 'POST',
+            headers: {'Content-Type': 'application/json'},
+            body: body,
+        })
+        .then(function(rsp) { rsp.json() })
+        .then(function(json) {
+            if (json.error && json.error.message) {
+            throw new Error(json.error.message);
+        }
+        return json;
+    });
+}
+
+
+module.export = utils
